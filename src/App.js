1f0b1ceab51c0e1caf2505140ddc097a42bbafc5
SkyeOnSol <130173026+SkyeOnSol@users.noreply.github.com>
2023-04-13 18:52:38 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,53 @@ import {
 } from "react-router-dom";
 import WalletSelect from "./pages/WalletSelect";
 import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
+
+import {
+  ConnectionProvider, WalletProvider,
+} from "@solana/wallet-adapter-react";
+
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+
+import {
+  PhantomWalletAdapter , SolflareWalletAdapter ,BraveWalletAdapter , BackpackWalletAdapter , LedgerWalletAdapter, ExodusWalletAdapter
+} from "@solana/wallet-adapter-wallets";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+import Main from "./Main";
+import { RPC_ENDPOINT } from "../utils";
+
+export default function Home() {
+  const [mounted, setMounted] = useState(false);
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new BraveWalletAdapter(),
+      new BackpackWalletAdapter(),
+      new LedgerWalletAdapter(),
+      new ExodusWalletAdapter(),
+    ],
+    []
+  );
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <ConnectionProvider
+      endpoint={RPC_ENDPOINT}
+      config={{ commitment: "confirmed" }}
+    >
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{mounted && <Main />}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
 
 function App() {
   const action = useNavigationType();
